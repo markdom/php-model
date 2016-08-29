304028eb414799f71449992d7ca9a85f96c6824e
Martin Brecht-Precht <mb@markenwerk.net>
2016-08-29 11:20:40 +0200

    Added test case for the DebugHandler.

---
+++ test/DebugTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Markdom\Test;
+
+use Markdom\Dispatcher\JsonDispatcher;
+use Markdom\Handler\DebugHandler;
+use Markdom\Model\Handler\ModelHandler;
+
+/**
+ * Class DebugTest
+ *
+ * @package Markdom\Test
+ */
+class DebugTest extends \PHPUnit_Framework_TestCase
+{
+
+	public function testParseHandle()
+	{
+		// Dispatch a JSON file as Markdom Document
+		$handler = new ModelHandler();
+		$dispatcher = new JsonDispatcher($handler);
+		$dispatcher->process(file_get_contents(__DIR__ . '/test-data.json'));
+		$document = $handler->getResult();
+
+		// Dispatch the Markdom Document as JSON string
+		$handler = new DebugHandler();
+		$document->handle($handler);
+		$debugString = $handler->getResult();
+		$this->assertEquals(file_get_contents(__DIR__ . '/test-data.debug.txt'), $debugString);
+	}
+
+}
