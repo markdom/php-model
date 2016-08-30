58842bc649d19332a1bca0654ba5b812de55e31b
Martin Brecht-Precht <mb@markenwerk.net>
2016-08-30 14:02:47 +0200

    Added test for Commonmark

---
+++ test/CommonmarkTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Markdom\Test;
+
+use Markdom\Dispatcher\JsonDispatcher;
+use Markdom\Handler\CommonmarkHandler;
+use Markdom\Model\Handler\ModelHandler;
+
+/**
+ * Class CommonmarkTest
+ *
+ * @package Markdom\Test
+ */
+class CommonmarkTest extends \PHPUnit_Framework_TestCase
+{
+
+	public function testParseHandle()
+	{
+		// Dispatch a JSON file as Markdom Document
+		$handler = new ModelHandler();
+		$dispatcher = new JsonDispatcher($handler);
+		$dispatcher->process(file_get_contents(__DIR__ . '/test-data.json'));
+		$document = $handler->getResult();
+
+		// Dispatch the Markdom Document as Commonmark string
+		$handler = new CommonmarkHandler();
+		$document->handle($handler);
+		$commonmarkString = $handler->getResult();
+		$this->assertEquals(file_get_contents(__DIR__ . '/test-data.md'), $commonmarkString);
+	}
+
+}
